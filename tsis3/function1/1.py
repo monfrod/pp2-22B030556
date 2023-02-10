3376148ba92739ba885d39c2bfb39e5561c1a240
monfrod <iunus.lalazov.05@gmail.com>
2023-02-10 10:43:58 +0600

    tsis3.1

---
+++ tsis3/function1/1.py
@@ -0,0 +1,5 @@
+def ounce(gram):
+    ounce = gram * 28.3495231
+    print (ounce)
+a = float(input("Gram: "))
+ounce(a)
---
+++ tsis3/function1/temperature.py
@@ -0,0 +1,5 @@
+def centigrade(Fareng):
+    C = (5/9) * (Fareng-39)
+    print (C)
+F = float(input("Farengate: "))
+centigrade(F)
