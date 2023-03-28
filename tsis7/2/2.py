028f19e07d363462429174ca4fa417a66550f7df
monfrod <iunus.lalazov.05@gmail.com>
2023-03-28 15:41:37 +0600

    tsis7.2

--- tsis7/2/2.py
+++ tsis7/2/2.py
@@ -1,31 +1,33 @@
 import pygame
 clock = pygame.time.Clock()
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
-sound = [pygame.mixer.Sound("Рингтон - Пиу пиу пиу.mp3"),
+screen = pygame.display.set_mode((1000, 500))
+sound = [pygame.mixer.Sound("INTERWORLD_-_METAMORPHOSIS_Sped_Up_(musmore.com).mp3"),
+         pygame.mixer.Sound("kordhell-murder-in-my-mind-muzonov.net_456239182.mp3"),
          pygame.mixer.Sound("2_pac_BIG_SYKE_-_All_eyez_on_me_(DJ_Belite_remix)_(ringon.site).mp3"),
          pygame.mixer.Sound("ZODIVK_-_Devil_eyes_Slowed_(ringon.site).mp3"),
          pygame.mixer.Sound("DNDM_-_Eastern_love_(Original_mix)_(ringon.site).mp3")]
 running = True
 i = 0
+sound[i].play()
 while running:
-
+    screen.fill("White")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
-                sound[i].stop
-                i+=1
-                sound[i].play
-            if event.key == pygame.K_a:
-                sound[i].stop
-                i-=1
-                sound[i].play
-            if event.key == pygame.K_w:
-                sound[i].play()
-            if event.key == pygame.K_s:
                 sound[i].stop()
-    if i > len(sound) or i < len(sound):
-        i = 0
+                i = i+1
+                if i > len(sound)-1:
+                    i = 0
+                sound[i].play()
+            elif event.key == pygame.K_a:
+                sound[i].stop()
+                i = i-1
+                if i < 0:
+                    i = len(sound)-1
+                sound[i].play()
+
+    pygame.display.flip()
     clock.tick(60)
